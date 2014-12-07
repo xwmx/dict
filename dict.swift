5f93144dad9d7b64e525da54ab9401e1a6a9b8d0
William Melody <hi@williammelody.com>
2014-12-06 16:28:09 -0800

    Using 4 spaces for indentation
    This appears to be the consensus, and it's the setting in swift.vim

--- dict.swift
+++ dict.swift
@@ -13,63 +13,63 @@ import Foundation
 
 // Construct the help messaged when given a program name.
 func helpMessageAs(programName: String) -> String {
-  let message = "Usage: \(programName) [options] word ...\n" +
-             "  -h  print these usage instructions"
-  return message
+    let message = "Usage: \(programName) [options] word ...\n" +
+                  "  -h  print these usage instructions"
+    return message
 }
 
 func errorMessageDataFor(word: String) -> NSData {
-  let errorMessage: NSString = "dict: no definition for \(word)\n"
-  let errorMessageData: NSData! = errorMessage.dataUsingEncoding(NSUTF8StringEncoding)
-  return errorMessageData
+    let errorMessage: NSString = "dict: no definition for \(word)\n"
+    let errorMessageData: NSData! = errorMessage.dataUsingEncoding(NSUTF8StringEncoding)
+    return errorMessageData
 }
 
 // When passed valid input, either print the definition of the word or, if no
 // definition for the word is present, print and error stating that.
 func printDefinitionFor(word: NSString) {
-  let wlen = DCSGetTermRangeInString(nil, word, 0)
-  let trimmedWord = word.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
-  if trimmedWord.isEmpty {
-    // do nothing when passed empty string
-  } else if wlen.location == -1 {
-    let stderr = NSFileHandle.fileHandleWithStandardError()
-    stderr.writeData(errorMessageDataFor(trimmedWord))
-  } else {
-    let wordDefinition = DCSCopyTextDefinition(nil, trimmedWord, wlen).takeUnretainedValue()
-    println(wordDefinition)
-  }
+    let wlen = DCSGetTermRangeInString(nil, word, 0)
+    let trimmedWord = word.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    if trimmedWord.isEmpty {
+         // do nothing when passed empty string
+    } else if wlen.location == -1 {
+        let stderr = NSFileHandle.fileHandleWithStandardError()
+        stderr.writeData(errorMessageDataFor(trimmedWord))
+    } else {
+        let wordDefinition = DCSCopyTextDefinition(nil, trimmedWord, wlen).takeUnretainedValue()
+        println(wordDefinition)
+    }
 }
 
 // Handle STDIN.
 func input() -> String {
-  let keyboard = NSFileHandle.fileHandleWithStandardInput()
-  let inputData = keyboard.availableData
-  let inputString: NSString! = NSString(data: inputData, encoding:NSUTF8StringEncoding)
-  let trimmedString = inputString.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
-  return trimmedString
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let inputString: NSString! = NSString(data: inputData, encoding:NSUTF8StringEncoding)
+    let trimmedString = inputString.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+    return trimmedString
 }
 
 // Extract the program name from the arguments, then iterate over the
 // arguments to determine what to do.
 func run() {
-  var arguments = Process.arguments
-  // Get program name
-  let programName = arguments[0].lastPathComponent
-  // Remove program name from argument list
-  arguments.removeAtIndex(0)
+    var arguments = Process.arguments
+    // Get program name
+    let programName = arguments[0].lastPathComponent
+    // Remove program name from argument list
+    arguments.removeAtIndex(0)
 
-  if arguments.isEmpty {
-    printDefinitionFor(input())
-  } else {
-    for argument in arguments {
-      switch argument {
-      case "-h":
-        println(helpMessageAs(programName))
-      default:
-        printDefinitionFor(argument);
-      }
+    if arguments.isEmpty {
+        printDefinitionFor(input())
+    } else {
+        for argument in arguments {
+            switch argument {
+            case "-h":
+                println(helpMessageAs(programName))
+            default:
+                printDefinitionFor(argument);
+            }
+        }
     }
-  }
 }
 
 // Entry point
